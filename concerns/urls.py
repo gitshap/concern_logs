cdb8201989f46db3c2c3dbb1ea4eef1afa0b4b38
Carl <cruzthecarl10@gmail.com>
2021-09-27 21:08:39 +0800

    added pdf export

--- concerns/urls.py
+++ concerns/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from concerns.views import (create_concern, create_concern_form, concern_detail, update_concern,
-                            delete_concern, home, empty, some_view)
+                            delete_concern, home, empty, some_view, export_pdf)
 
 urlpatterns = [
     path('create_concern/', create_concern, name='create-concern'),
     path('home/', home, name='home'),
     path('empty/', empty, name='empty'),
     path('csv/', some_view, name='csv'),
+    path('pdf/', export_pdf, name='pdf'),
     path('htmx/create-book-form/', create_concern_form,
          name='create-concern-form'),
     path('htmx/concern/<pk>/', concern_detail, name="concern-detail"),
--- concerns/views.py
+++ concerns/views.py
@@ -2,14 +2,44 @@ from django.http import HttpResponse, HttpResponseNotAllowed
 from django.shortcuts import get_object_or_404, redirect, render
 from concerns.models import Concerns
 from concerns.forms import ConcernCreationForm
-from django.template import loader
+import io
+from django.http import FileResponse
+from reportlab.pdfgen import canvas
+
 
 import csv
+
+def export_pdf(request):
+    # Create a file-like buffer to receive PDF data.
+    buffer = io.BytesIO()
+
+    # Create the PDF object, using the buffer as its "file."
+    p = canvas.Canvas(buffer)
+
+    # Draw things on the PDF. Here's where the PDF generation happens.
+    # See the ReportLab documentation for the full list of functionality.
+    concern = Concerns.objects.get(id=33)
+    p.drawString(200, 700, concern.person)
+    p.drawString(300, 700, concern.findings)
+    p.drawString(400, 700, concern.status)
+
+    
+
+    # Close the PDF object cleanly, and we're done.
+    p.showPage()
+    p.save()
+
+    # FileResponse sets the Content-Disposition header so that browsers
+    # present the option to save the file.
+    buffer.seek(0)
+    return FileResponse(buffer, as_attachment=True, filename='hello.pdf')
+
+
 
 def some_view(request):
     # Create the HttpResponse object with the appropriate CSV header.
     response = HttpResponse(
-        content_type='text/csv',
+        content_type='text/csv/force-download',
         headers={'Content-Disposition': 'attachment; filename="somefilename.csv"'},
     )
 
