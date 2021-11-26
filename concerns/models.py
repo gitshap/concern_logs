26ff3247ffbc9bfaa104746e9897f8f958853565
Carl <shapit@SHAP.EDU.PH>
2021-11-26 11:11:58 +0800

    fixed time bug

--- concerns/models.py
+++ concerns/models.py
@@ -19,8 +19,8 @@ class Concerns(models.Model):
     status = models.CharField(
         max_length=20, choices=STATUS_CHOICES, default=DONE, blank=True)
     additional_notes = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=timezone.now)
-    updated_at = models.DateTimeField(auto_now=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.person}'s problem is {self.status}"
